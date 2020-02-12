8b2f92fd84aaf88f1fdf78ade0c757fee18920f7
Kavitha Gowri Pasupuleti <kavitha.pasupuleti@imaginea.com>
2020-02-12 12:50:31 +0530

    step 3

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ var app = new Vue({
     el: '#app',
     data: {
         appTitle: 'To do app',
-        value: 'kavitha'
+        toDoValue: '',
+        toDoList: ['one','two','three','four'],
+        count : 0
+    },
+    
+    methods:{
+        addItem : function(){
+            this.toDoList.push(this.toDoValue)
+            this.toDoValue = ''
+        },
+        addBtn : function(){
+            this.count += 1;
+        },
+        deleteBtn : function(){
+            if(this.count>0)
+            this.count -= 1;
+        }
     }
   })
